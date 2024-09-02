3ace8329a9c44b0928866281137c39230a4cde1d
benil09 <benil8235@gmail.com>
2024-09-02 16:00:27 +0530

    first commit

---
+++ lab1.py
@@ -0,0 +1,25 @@
+# write a program to perform  (i)Addition (ii)Subrtaction (iii)Multiplication (iv)Division (v) integer quotient
+# (vi) integer remainder 
+
+
+
+a= int (input("enter first number:"))
+b= int (input("enter second number:"))
+
+sum=a+b
+print("sum of two numbers is:",sum)
+
+diff=a-b
+print("difference of two numbers is:",diff)
+
+product = a*b
+print("product of two numbers is:",product)
+
+division=a/b
+print("division of two numbers is:",division)
+
+integerQuotient=a//b
+print("integer quotient of two numbers is:",integerQuotient)
+
+integerRemainder=a%b
+print("integer remainder of two number is: ",integerRemainder)
